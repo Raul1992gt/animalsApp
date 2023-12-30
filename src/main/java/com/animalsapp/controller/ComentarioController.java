344dd64e4f904ea14f3e3026d713e6da512d8606
Raul Garcia <rgarciat94@gmail.com>
2023-12-30 11:55:32 +0100

    añadir RestController a los que faltaban.

--- src/main/java/com/animalsapp/controller/ComentarioController.java
+++ src/main/java/com/animalsapp/controller/ComentarioController.java
@@ -1,4 +1,8 @@
 package com.animalsapp.controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/comentario")
 public class ComentarioController {
 }
--- src/main/java/com/animalsapp/controller/UsuarioController.java
+++ src/main/java/com/animalsapp/controller/UsuarioController.java
@@ -1,4 +1,8 @@
 package com.animalsapp.controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/usuario")
 public class UsuarioController {
 }
